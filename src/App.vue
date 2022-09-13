645095f7241bcc76f52bb2fdc1512fcc118382ee
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-09-13 15:53:32 -0500

    my first component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Favorite Restaurant"/>
+  <CountButton/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import CountButton from "./components/CountButton";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CountButton
   }
 }
 </script>
---
+++ src/components/CountButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="count++">
+    Count is: {{ count }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "CountButton",
+  data(){
+  return {
+    count: 0
+  }}
+}
+</script>
+
+<style scoped>
+
+</style>
