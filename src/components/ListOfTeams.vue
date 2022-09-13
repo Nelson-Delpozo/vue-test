64516299690d0b1a556ab54a4e4863f778d02d55
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-09-13 17:38:48 -0500

    iterating over embedded arrays

--- src/components/ListOfTeams.vue
+++ src/components/ListOfTeams.vue
@@ -1,12 +1,16 @@
 <template>
-<h3 v-for="team in teams" :key="team">{{team}}</h3>
+  <div v-for="team in teams" :key="team.name">
+<h3>{{team.name}}</h3>
+  <h5 v-for="driver in team.drivers" :key="driver">{{driver}}</h5>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ListOfTeams",
   data(){
-    return{ teams: ["Alfa-Romeo", "Ferrari", "Red Bull", "Alpha-Tauri", "McLaren", "Haas", "Mercedes-Benz", "Alpine", "Williams", "Aston-Martin"]
+    return{
+      teams: [{name:"Alfa-Romeo", drivers:["Bottas", "Zhou"]}, {name:"Ferrari", drivers:["LeClerc", "Sainz"]}, {name:"Red Bull", drivers:["Verstappen", "Perez"]}, {name:"Alpha Tauri", drivers:["Gasley", "Tsunoda"]}, {name:"McLaren", drivers:["Ricciardo", "Norris"]}, {name:"Haas", drivers:["Schumacher", "Magnussen"]}, {name:"Mercedes-Benz", drivers:["Lewis", "Russell"]}, {name:"Alpine", drivers:["Alonso", "Ocon"]}, {name:"Williams", drivers:["DeVries", "Albon"]}, {name:"Aston Martin", drivers:["Stroll", "Vettel"]}]
   }}
 
 }
